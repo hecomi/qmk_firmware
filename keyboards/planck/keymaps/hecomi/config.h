fb16e9a077ebea6dbca6e5bf68d36ad5727b1c75
hecomi <info@hecomi.com>
2019-03-31 12:49:27 +0900

    fix my planck settings.

--- keyboards/planck/keymaps/hecomi/config.h
+++ keyboards/planck/keymaps/hecomi/config.h
@@ -37,3 +37,13 @@
 // Most tactile encoders have detents every 4 stages
 #define ENCODER_RESOLUTION 4
 
+
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 100
+
+#define MOUSEKEY_DELAY             50
+#define MOUSEKEY_INTERVAL          4
+#define MOUSEKEY_MAX_SPEED         1
+#define MOUSEKEY_TIME_TO_MAX       20
+#define MOUSEKEY_WHEEL_MAX_SPEED   2
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 20
